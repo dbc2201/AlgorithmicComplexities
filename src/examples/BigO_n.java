b3e7db7100f29105784f7b0b28339e00ded3d473
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:34:26 +0530

    complete printArray()

--- src/examples/BigO_n.java
+++ src/examples/BigO_n.java
@@ -18,7 +18,14 @@ public class BigO_n {
      * @param array the input array to print
      */
     public static void printArray(int[] array) {
+        /*
+         * This for loop will run as many times as the length of the array.
+         * So, if we suppose that the length of the array is 'n', then the loop
+         * will also run 'n' times.
+         * Hence, the complexity O(n)
+         * */
         for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + (i < array.length - 1 ? ", " : ""));
         }
     }
 }
