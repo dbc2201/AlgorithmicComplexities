3f3027f17abf3c7720fc0d2b400ba5ee133ee42f
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:50:44 +0530

    create method showComplexity()

--- src/examples/BigO_nlogn.java
+++ src/examples/BigO_nlogn.java
@@ -8,4 +8,11 @@
 package examples;
 
 public class BigO_nlogn {
+    public static void showComplexity(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < 8; j = j * 2) {
+                System.out.println("i = " + i + " j = " + j);
+            }
+        }
+    }
 }
