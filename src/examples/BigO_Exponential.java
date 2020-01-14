c8828cc255e91a980b6cb167e9e67a063baa6c7d
dbc2201 <divyanshb22@gmail.com>
2020-01-14 09:02:34 +0530

    add JavaDoc comment for showComplexity()

--- src/examples/BigO_Exponential.java
+++ src/examples/BigO_Exponential.java
@@ -8,6 +8,15 @@
 package examples;
 
 public class BigO_Exponential {
+    /**
+     * This method runs 2^n times for every input,
+     * for example, if the value of n was 4, it would run 16 times
+     * but if the value of n was 0, it would still run, but only 1 times.
+     * Hence we can see that the number of iteration depend upon the formula
+     * 2^n, hence the complexity of this algorithms is O(2^n),
+     * We can change this to a more general time, by replacing the 2 in the Math.pow(2, n)
+     * to a 'k', then the complexity of this algorithm would be O(k^n).
+     */
     public static void showComplexity(int n) {
         for (int i = 0; i < Math.pow(2, n); i++) {
             System.out.print("i = " + i + " ");
