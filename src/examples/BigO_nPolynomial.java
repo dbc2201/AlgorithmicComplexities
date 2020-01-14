9b36a46c8502acc230edad9c0f350e157c46cd51
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:54:50 +0530

    create method printTwoDimensionalArray()

--- src/examples/BigO_nPolynomial.java
+++ src/examples/BigO_nPolynomial.java
@@ -8,4 +8,13 @@
 package examples;
 
 public class BigO_nPolynomial {
+    private static int[][] twoDimensionalArray = new int[3][3];
+
+    public static void printTwoDimensionalArray() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(twoDimensionalArray[i][j]);
+            }
+        }
+    }
 }
