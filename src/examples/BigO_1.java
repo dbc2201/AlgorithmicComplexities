1c3b2b7dcce1bee236f77034c331ac984804fb91
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:40:53 +0530

    add JavaDoc comment for checkEvenOrOdd()

--- src/examples/BigO_1.java
+++ src/examples/BigO_1.java
@@ -8,6 +8,13 @@
 package examples;
 
 public class BigO_1 {
+    /**
+     * This method does take an input, but the number of steps taken by the
+     * method does not depend upon the value of the input.
+     * Hence, we can say, that is will always take the same (constant) amount of time
+     * to run for different values.
+     * Hence, the complexity of this method is O(1).
+     */
     public static void checkEvenOrOdd(int n) {
         System.out.println((n % 2 == 0) ? "Even" : "Odd");
     }
