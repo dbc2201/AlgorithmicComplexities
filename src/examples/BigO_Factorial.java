4ce7cadb27cbb9f0fec3a746e7b974b6e665da0d
dbc2201 <divyanshb22@gmail.com>
2020-01-14 09:04:10 +0530

    create method factorial() to calculate factorial using recursion

--- src/examples/BigO_Factorial.java
+++ src/examples/BigO_Factorial.java
@@ -8,4 +8,10 @@
 package examples;
 
 public class BigO_Factorial {
+    public static int factorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
 }
