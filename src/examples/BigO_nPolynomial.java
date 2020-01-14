32cd6d2733becc96ab1cc50d6e8a5eb1b2883e1c
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:56:38 +0530

    add JavaDoc comment for printTwoDimensionalArray()

--- src/examples/BigO_nPolynomial.java
+++ src/examples/BigO_nPolynomial.java
@@ -10,6 +10,14 @@ package examples;
 public class BigO_nPolynomial {
     private static int[][] twoDimensionalArray = new int[3][3];
 
+    /**
+     * As we can see, both the loops run n times, hence both of them have a
+     * complexity of O(n), so when we multiply these complexities together we get,
+     * O(n x n) OR O(n^2).
+     * It would be true for any number of nested loops, if we add another loop inside
+     * the 'j' loop that also runs 'n' times, then the total complexity of this
+     * algorithm would be O(n^3)
+     */
     public static void printTwoDimensionalArray() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
