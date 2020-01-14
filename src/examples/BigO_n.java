53dadb40776e1c083c9fd5cd49c700f99decc495
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:29:33 +0530

    create class BigO_n

---
+++ src/examples/BigO_n.java
@@ -0,0 +1,15 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: divyanshb
+ *   Date: 14/01/20
+ *   Time: 8:27 AM
+ */
+
+package examples;
+
+/**
+ * This class will show an example for an algorithm that has a time complexity of
+ * O(n) - Big Oh of n.
+ */
+public class BigO_n {
+}
