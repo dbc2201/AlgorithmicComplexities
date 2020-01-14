06bbe3d3ce9d654da9667e8f64f742127661c197
dbc2201 <divyanshb22@gmail.com>
2020-01-14 09:00:08 +0530

    create method create method showComplexity()

--- src/examples/BigO_Exponential.java
+++ src/examples/BigO_Exponential.java
@@ -8,4 +8,9 @@
 package examples;
 
 public class BigO_Exponential {
+    public static void showComplexity(int n) {
+        for (int i = 0; i < Math.pow(2, n); i++) {
+            System.out.print("i = " + i + " ");
+        }
+    }
 }
