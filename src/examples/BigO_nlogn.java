a4f425142315cbf4dde90dfa0508519c0c96cb24
dbc2201 <divyanshb22@gmail.com>
2020-01-14 08:52:34 +0530

    add JavaDoc comment for showComplexity()

--- src/examples/BigO_nlogn.java
+++ src/examples/BigO_nlogn.java
@@ -8,6 +8,15 @@
 package examples;
 
 public class BigO_nlogn {
+    /**
+     * In this method, we can see that the first loop would be running
+     * n times, hence the complexity of that loop is O(n)
+     * But, the second loop is running only logn times, hence its complexity is
+     * O(logn)
+     * To calculate the total complexity of this algorithm, we will multiply the
+     * two complexities together
+     * Hence, we achieve O(n x logn) OR O(nlogn)
+     */
     public static void showComplexity(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j < 8; j = j * 2) {
