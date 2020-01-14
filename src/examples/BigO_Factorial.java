6f84e31b923473fa5359b40be22f7e57a25919e8
dbc2201 <divyanshb22@gmail.com>
2020-01-14 09:06:52 +0530

    add Ja

--- src/examples/BigO_Factorial.java
+++ src/examples/BigO_Factorial.java
@@ -15,6 +15,11 @@ public class BigO_Factorial {
         return n * factorial(n - 1);
     }
 
+    /**
+     * As we can see in the code, the number of step for the loop
+     * depend upon the formula n! (n - factorial), Hence we can say that the
+     * algorithmic complexity of this method is O(n!).
+     */
     public static void showComplexity(int n) {
         for (int i = 0; i < factorial(4); i++) {
             System.out.print("i = " + i + " ");
